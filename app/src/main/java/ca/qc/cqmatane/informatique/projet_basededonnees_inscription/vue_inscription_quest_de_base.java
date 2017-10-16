eff932c1edfe2207430df0c4e3228bb885aaf787
Frelin Maxime <frelinmaxime@gmail.com>
2017-10-16 18:31:07 +0200

    Initial Projet

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/vue_inscription_quest_de_base.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vue_inscription_quest_de_base extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_quest_de_base);
+    }
+}
