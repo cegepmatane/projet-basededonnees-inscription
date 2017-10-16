299b55216c13b5b9d63d660f33f6b4b474b84539
Frelin Maxime <frelinmaxime@gmail.com>
2017-10-16 18:46:09 +0200

    Ajout de l'activité pour choisir la date aller retour

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/vue_inscription_date_aller_retour.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vue_inscription_date_aller_retour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_date_aller_retour);
+    }
+}
