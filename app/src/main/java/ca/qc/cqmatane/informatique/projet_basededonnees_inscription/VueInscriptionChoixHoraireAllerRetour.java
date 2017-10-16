e5cbd3083fbe5c63da3ee4ed8f1407bd6b11d6d9
ppenelon <penelon.pierre@gmail.com>
2017-10-16 13:08:47 -0400

    Ajout des vues pour choisir les horaires du voyage

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionChoixHoraireAllerRetour.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueInscriptionChoixHoraireAllerRetour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_choix_horaire_aller_retour);
+    }
+}
---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionChoixHoraireAllerSimple.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueInscriptionChoixHoraireAllerSimple extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_choix_horaire_aller_simple);
+    }
+}
