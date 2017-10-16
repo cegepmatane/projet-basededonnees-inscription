9479d014d75a8780be37cf95c9d1667d03d2b0bf
lucas <lucas.paulin@hotmail.fr>
2017-10-16 15:35:03 -0400

    Dépendence des radioButton

--- app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionQuestionDeBase.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionQuestionDeBase.java
@@ -3,13 +3,33 @@ package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class VueInscriptionQuestionDeBase extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_inscription_quest_de_base);
+        setContentView(R.layout.vue_inscription_question_de_base);
+
+        RadioButton radioButtonAllerSimple = (RadioButton) findViewById(R.id.bouton_aller_simple);
+        radioButtonAllerSimple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton radioButtonAllerRetour = (RadioButton) findViewById(R.id.bouton_aller_retour);
+                radioButtonAllerRetour.setChecked(false);
+            }
+        });
+
+        RadioButton radioButtonAllerRetour = (RadioButton) findViewById(R.id.bouton_aller_retour);
+        radioButtonAllerRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton radioButtonAllerSimple = (RadioButton) findViewById(R.id.bouton_aller_simple);
+                radioButtonAllerSimple.setChecked(false);
+            }
+        });
 
     }
 }
