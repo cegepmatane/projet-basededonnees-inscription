b90842b929917ae4b633d266e17edbf6711b5e43
Frelin Maxime <frelinmaxime@gmail.com>
2017-10-18 20:53:38 +0200

    Finalisation de la date aller-retour et ajout de la classe pour avoir le ticket

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionTicket.java
@@ -0,0 +1,15 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueInscriptionTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_ticket);
+
+    }
+
+}
