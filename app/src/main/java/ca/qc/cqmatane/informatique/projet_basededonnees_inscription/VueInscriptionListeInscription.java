36e439cbebf4867ec7558ef21b0d604518a33931
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-10-31 22:37:16 -0400

    Ajout d'une vue pour faire la liste des inscriptions

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionListeInscription.java
@@ -0,0 +1,13 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueInscriptionListeInscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_liste_inscription);
+    }
+}
