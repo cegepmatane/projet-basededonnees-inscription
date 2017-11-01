cb148537121d17bc549f9606d07cf22077ea1b35
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-11-01 03:48:41 -0400

    Continuation de la vue pour créer une liste des billets

--- app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionListeInscription.java
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionListeInscription.java
@@ -2,12 +2,29 @@ package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ca.qc.cqmatane.informatique.projet_basededonnees_inscription.outils.ConnexionHTTP;
+import ca.qc.cqmatane.informatique.projet_basededonnees_inscription.outils.GestionXML;
 
 public class VueInscriptionListeInscription extends AppCompatActivity {
+
+    ListView listeInscriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_inscription_liste_inscription);
+
+        listeInscriptions = (ListView)findViewById(R.id.liste_inscription);
+
+        ConnexionHTTP connexionHTTP = new ConnexionHTTP(this, "RecupererInscription.php", listeInscriptions);
+        connexionHTTP.execute();
     }
+
 }
