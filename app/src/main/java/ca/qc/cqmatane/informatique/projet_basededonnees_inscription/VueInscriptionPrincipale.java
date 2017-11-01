c22b8019b9912bc1d5f414242f94ae1e5e9edc26
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-10-31 22:31:49 -0400

    Ajout de la vuePrincipale

---
+++ app/src/main/java/ca/qc/cqmatane/informatique/projet_basededonnees_inscription/VueInscriptionPrincipale.java
@@ -0,0 +1,22 @@
+package ca.qc.cqmatane.informatique.projet_basededonnees_inscription;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class VueInscriptionPrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_inscription_principale);
+    }
+
+    public void actionAfficherInscriptions(View vue){
+
+    }
+
+    public void actionFaireInscription(View vue){
+
+    }
+}
